98759629c7e100816cc582a0706d26a90b054afb
Andrew Ferreira <andrew.shien2@gmail.com>
2026-04-20 19:40:34 -0300

    feat: search by name

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,8 +1,15 @@
 use anyhow::Result;
+use clap::Parser;
+
+#[derive(Parser)]
+struct Cli {
+  name: String,
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
-  let cards = ygo_core::fname("Dark Magician").await?;
+  let args = Cli::parse();
+  let cards = ygo_core::fname(&args.name).await?;
   println!("{}", serde_json::to_string_pretty(&cards)?);
   Ok(())
 }
